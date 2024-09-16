d5c7e14701cbc9bdf33eae89fb8abe63b48f2e30
mustafa daglilar <m.daglilar@hotmail.com>
2024-09-16 21:40:20 +0300

    fixed consumer-services

--- LibraryManagementSystem.Messaging/RabbitMQConsumer.cs
+++ LibraryManagementSystem.Messaging/RabbitMQConsumer.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LibraryManagementSystem.Entities
+namespace LibraryManagementSystem.Messaging
 {
     public class RabbitMQConsumer
     {
